7e9bcd6f12ce262e841c475544b70ffe43cd4188
Sieun Kim <se0983@kookmin.ac.kr>
2022-01-02 01:09:41 +0900

    A/B

---
+++ BOJ/1008.java
@@ -0,0 +1,16 @@
+# 1008 - A/B
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+				
+		Scanner in = new Scanner(System.in);
+		
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		
+		System.out.println(a/b);
+		in.close();
+	}
+}
